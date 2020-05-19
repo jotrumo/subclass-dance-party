898492d0aaffcedff922f20f1ff5bda2f2f40af3
jotrumo <jtmontoya26@gmail.com>
2020-05-19 10:53:06 -0700

    All dancer class created - all test passing.

---
+++ spec/growyDancerSpec.js
@@ -0,0 +1,33 @@
+describe('growyDancer', function() {
+
+  var growyDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    growyDancer = new gDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(growyDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it('should have a step function that makes its node shake', function() {
+    sinon.spy(growyDancer.$node, 'animate');
+    growyDancer.step();
+    expect(growyDancer.$node.animate.called).to.be.true;
+  });
+
+  it('should call step at least once per second', function() {
+    sinon.spy(growyDancer, 'step');
+    //console.log(blinkyDancer, blinkyDancer.step.callCount)
+    clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+    clock.tick(timeBetweenSteps);
+
+
+    expect(growyDancer.step.callCount).to.be.equal(1);
+
+    clock.tick(timeBetweenSteps);
+    expect(growyDancer.step.callCount).to.be.equal(2);
+  });
+});
